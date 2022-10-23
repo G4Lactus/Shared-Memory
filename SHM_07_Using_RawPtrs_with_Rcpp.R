28e177f07a3661d2cd69d13f0cec496fd16508e5
G4Lactus <F-Scheidt@gmx.de>
2022-10-23 19:54:03 +0200

    Update SHM 07

--- SHM_07_Using_RawPtrs_with_Rcpp.R
+++ SHM_07_Using_RawPtrs_with_Rcpp.R
@@ -1,7 +1,5 @@
 # Starting from this part we want to make active use of shared memory by means
 # of pointers and C++. We start with C++ raw pointers.
-# 
-# 
 # ------------------------
 library(Rcpp)
 sourceCpp("SHM_07_Using_RawPtrs_with_Rcpp.cpp")
@@ -13,16 +11,9 @@ print(A)
 
 # we define the function return_mat() in Rcpp, take it as a reference from R
 # and beam it as a pointed to variable through some pipeline.
-return_rawPtr_to_A(A)
-print(A)                # though we worked with a reference no changes are in A
-                        # because we defined a new pointer to the data.
-
-return_A(A)
+simulate_rawPtr_Usage(A, 2)
 print(A)
-
-B <- return_A(A)
-print(B)
-B <- B + 1
-print(B)
+# though we worked with a reference no changes are in A because we defined a
+# new pointer to the data.
+simulate_rawPtr_Usage(A, 1)
 print(A)
-
