f79a8564c75373e18396a5f5744e5b131d5a5182
Fabian Scheidt <f-scheidt@gmx.de>
2025-07-16 18:48:31 +0200

    Update SHM_07

---
+++ SHM_07_RawPtrs_Cpp.R
@@ -0,0 +1,49 @@
+#' This tutorial explores how C++ pointers intereact with R objects via Rcpp.
+#' Two main principles under investigation:
+#'  1. How modifications in C++ are returned to R.
+#'  2. The dangers of manual memory allocation ('new') in C++.
+# ------------------------------------------------------------------------------
+#'
+library(Rcpp)
+sourceCpp("SHM_07_RawPtrs_Cpp.cpp")
+
+#' Setup
+#' Create our original matrix in R
+set.seed(42)
+A_orig <- matrix(rnorm(12), nrow = 4, ncol = 3)
+print(A_orig)
+
+
+
+#' Experiment 1: Modifying by reference and returning a copy
+# ------------------------------------------------------------------------
+cat("\nExperiment 1:\n")
+#' Call the C++ function that modifies the object it receives per reference
+#' and returns the result
+B_mod <- simulate_pointer_behavior(A_orig, 1)
+
+#' Result:
+cat("Original 'A_orig' is UNCHANGED:\n")
+print(A_orig)
+cat("The returned matrix 'B_mod' contains the new data:\n")
+print(B_mod)
+#' The C++ code created a copy and changed the copied version.
+#' The output is a completely new object, the original data 'A_orig' remain.
+
+
+
+#' Experiment 2: Modifying via a reference pointer
+# ------------------------------------------------------------------------
+cat("\n Experiment 2:\n")
+#' Call the C++ function that copies `A_orig` data to a NEW matrix
+#'  on the heap, modifies it, and returns the copied and modified content.
+C_mod <- simulate_pointer_behavior(A_orig, 2)
+
+#' Result:
+cat("Original 'A_orig' is UNCHANGED:\n")
+print(A_orig)
+cat("The returned matrix 'C_mod' contains the modified data:\n")
+print(C_mod)
+#' Our C++ function simulate_pointer_behavior returns a *copy* of
+#'  the modified data as a new R object. The original R matrix is
+#'  not modified in place.
---
+++ SHM_07_RawPtrs_Cpp.cpp
@@ -0,0 +1,70 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+
+
+
+// This helper function modifies an Armadillo matrix passed by reference
+arma::mat modify_by_ref_ptr(arma::mat& A) {
+ 
+  Rcpp::Rcout << "Inside C++: Modifying via a pointer to the received object\n";
+
+  arma::mat Ac = A; // Copy input
+  Ac += 42;         // modify copy
+  return Ac;        // return modified copy
+}
+
+
+// This helper function demonstrates creating a NEW matrix in memory
+// 1. Copies the content of A.
+// 2. Allocates a new object on the heap.
+// 3. Modifies the new object.
+// 4. Returns a pointer to the new heap object.
+arma::mat* modify_by_new_ptr(arma::mat& A) {
+  Rcpp::Rcout << "Inside C++: Modifying a new object with 'new'\n";
+
+  // WARNING: MEMORY LEAK
+  // The 'new' keyword allocated fresh memory on teh heap that C++ will
+  // not automatically manage. The memory is owned by the program, and
+  // we are responsible for freeing with with 'delete' later on.
+  // As no delete is called, a memory leak occurs.
+
+  // new matrix on the heap, copying the data
+  arma::mat* ptr_to_new_mat = new arma::mat((double*) A.memptr(), A.n_rows, A.n_cols);
+
+  // modify content of the NEW matrix
+  *ptr_to_new_mat += 100;
+
+  // Return the pointer tot he newly allocated memory
+  return ptr_to_new_mat;
+}
+
+
+
+// [[Rcpp::export]]
+arma::mat simulate_pointer_behavior(arma::mat& A,int type) {
+
+  switch (type) {
+    case 1: {
+      // Return a dereferenced pointer pointing towards A.
+      // Note, the original A is not modified, we return a copy.
+      return modify_by_ref_ptr(A);
+
+    };
+    case 2: {
+      // Memory Leak:
+      // we can't return the raw pointer, and have no way to
+      // 'delete' it later. The memory is orphaned.
+      return *modify_by_new_ptr(A);
+
+    };
+    default: {
+      throw Rcpp::exception("Sorry type not defined, just 1 or 2.");
+      break;
+    };
+  }
+}
+
+// NOTE: 
+// Returning a raw pointer to R fails, as it is no SEXP (S expression).
+// The code does not compile! Either you dereference the pointer or you make
+// an XPtr and return it to R.
